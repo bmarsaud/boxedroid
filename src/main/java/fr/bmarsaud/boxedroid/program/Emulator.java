2f1c9d8b6b6e4d343b5c96df70b84fb9afe18b97
Bastien Marsaud <bastien@mouseover.fr>
2020-02-23 17:44:44 +0100

    doc: add programs documentation

--- src/main/java/fr/bmarsaud/boxedroid/program/Emulator.java
+++ src/main/java/fr/bmarsaud/boxedroid/program/Emulator.java
@@ -2,11 +2,19 @@ package fr.bmarsaud.boxedroid.program;
 
 import java.io.IOException;
 
+/**
+ * An abstraction of the emulator program for the Android SDK
+ */
 public class Emulator extends Program {
     public Emulator(String path) {
         super(path);
     }
 
+    /**
+     * Run the launch command launching an AVD
+     * @param avdName The AVD name to launch
+     * @return The process of the executed command
+     */
     public Process launch(String avdName) throws IOException {
         return this.execute("-avd", avdName);
     }
--- src/main/java/fr/bmarsaud/boxedroid/program/Program.java
+++ src/main/java/fr/bmarsaud/boxedroid/program/Program.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * A abstraction of an external executable program
+ */
 public class Program {
     private String path;
 
@@ -12,6 +15,11 @@ public class Program {
         this.path = path;
     }
 
+    /**
+     * Execute the program with the given arguments
+     * @param args The arguments passed to the program
+     * @return The process of the executed program
+     */
     public Process execute(String... args) throws IOException {
         List<String> command = new ArrayList<>(Arrays.asList(args));
         command.add(0, path);
