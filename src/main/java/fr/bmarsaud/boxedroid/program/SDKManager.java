f3d22358cc523164b19d95175d35b8abab5a4b58
Bastien Marsaud <bastien@mouseover.fr>
2020-05-09 14:27:44 +0200

    refactor: use abi and platforms enums in sdk manager

--- src/main/java/fr/bmarsaud/boxedroid/program/SDKManager.java
+++ src/main/java/fr/bmarsaud/boxedroid/program/SDKManager.java
@@ -2,7 +2,9 @@ package fr.bmarsaud.boxedroid.program;
 
 import java.io.IOException;
 
+import fr.bmarsaud.boxedroid.entities.ABI;
 import fr.bmarsaud.boxedroid.entities.APILevel;
+import fr.bmarsaud.boxedroid.entities.Platform;
 
 /**
  * An abstraction of the SDKManager program from the Android SDK
@@ -24,12 +26,14 @@ public class SDKManager extends Program {
     }
 
     /**
-     * Run the command installing the system images with google apis of a certain API level
+     * Run the command installing the system images of a certain API level, ABI and platform
      * @param apiLevel The API level of the system images to install
+     * @param abi The ABI supported by the system images to install
+     * @param platform The platform of the system images to install
      * @return The process of the executed command
      */
-    public Process installSystemImage(APILevel apiLevel) throws IOException {
+    public Process installSystemImage(APILevel apiLevel, ABI abi, Platform platform) throws IOException {
         return this.execute("--sdk_root=" + sdkPath,
-                "system-images;android-" + apiLevel.getCode() + ";google_apis;x86");
+                "system-images;android-" + apiLevel.getCode() + ";" + platform.getId() +";" + abi.getId());
     }
 }
