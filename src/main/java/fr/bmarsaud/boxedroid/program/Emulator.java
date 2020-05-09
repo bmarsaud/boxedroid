b93ac962ff6981eddfb3a31220cf280dc8caec1d
Bastien Marsaud <bastien@mouseover.fr>
2020-05-09 17:22:35 +0200

    fix: add binding of sdk root to emulator program

--- src/main/java/fr/bmarsaud/boxedroid/program/Emulator.java
+++ src/main/java/fr/bmarsaud/boxedroid/program/Emulator.java
@@ -6,8 +6,11 @@ import java.io.IOException;
  * An abstraction of the emulator program for the Android SDK
  */
 public class Emulator extends Program {
-    public Emulator(String path) {
+    private String sdkPath;
+
+    public Emulator(String path, String sdkPath) {
         super(path);
+        this.sdkPath = sdkPath;
     }
 
     /**
@@ -16,6 +19,7 @@ public class Emulator extends Program {
      * @return The process of the executed command
      */
     public Process launch(String avdName) throws IOException {
-        return this.execute("-avd", avdName);
+        return this.addEnvironment("ANDROID_SDK_ROOT", sdkPath)
+                .execute("-avd", avdName);
     }
 }
