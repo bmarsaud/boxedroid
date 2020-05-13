5a41403a6f308168c3b5360cd2e6a44e5c74ca65
Bastien Marsaud <bastien@mouseover.fr>
2020-05-13 12:35:11 +0200

    feat: read stream as UTF-8

--- src/main/java/fr/bmarsaud/boxedroid/program/Program.java
+++ src/main/java/fr/bmarsaud/boxedroid/program/Program.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -141,7 +142,7 @@ public class Program extends Observable {
     private void observersListenToStream(ArrayList<Observer> observers, InputStream stream) {
         new Thread(() -> {
             String line;
-            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
 
             try {
                 while ((line = reader.readLine()) != null) {
