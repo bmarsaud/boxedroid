582f8b89697df71311a5977fe869579f83cd8385
Bastien Marsaud <bastien@mouseover.fr>
2020-05-16 16:15:45 +0200

    feat: add device list parser

---
+++ src/main/java/fr/bmarsaud/boxedroid/util/DeviceListParser.java
@@ -0,0 +1,87 @@
+package fr.bmarsaud.boxedroid.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
+import fr.bmarsaud.boxedroid.entity.Device;
+
+/**
+ * Parse the output of the AVD Manager list device command to a list of devices
+ */
+public class DeviceListParser {
+    private static final String DEVICE_SEPARATOR = "---------";
+    private static final String ID_TAG = "id:";
+    private static final String CODE_TAG = " or ";
+    private static final String NAME_TAG = "Name:";
+    private static final String OEM_TAG = "OEM :";
+    private static final String TAG_TAG = "Tag :";
+
+    private Observer observer;
+    private List<String> programOutput;
+
+    private List<Device> devices;
+
+    public DeviceListParser() {
+        this.devices = new ArrayList<>();
+        this.programOutput = new ArrayList<>();
+        this.observer = (program, obj) -> { // JS style <3
+            String line = (String) obj;
+            programOutput.add(line);
+        };
+    }
+
+    /**
+     * Parse the output of the given process
+     * @param process The AVDManager device list process
+     * @throws InterruptedException
+     */
+    public void parse(Process process) throws InterruptedException {
+        process.waitFor();
+
+        Device currentDevice = new Device();
+        for(String line : programOutput) {
+            if(line.contains(DEVICE_SEPARATOR)) {
+                devices.add(currentDevice);
+                currentDevice = new Device();
+                continue;
+            }
+
+            if(line.contains(ID_TAG)) {
+                line = line.replace(ID_TAG, "");
+                String[] idPair = line.split(CODE_TAG);
+                String id = IOUtils.removeSurroundingSpaces(idPair[0]);
+                String code = idPair[1].replaceAll("\"", "");
+
+                currentDevice.setId(Integer.parseInt(id));
+                currentDevice.setCode(code);
+            } else if(line.contains(NAME_TAG)) {
+                String name = IOUtils.removeSurroundingSpaces(line.split(NAME_TAG)[1]);
+                currentDevice.setName(name);
+            } else if(line.contains(OEM_TAG)) {
+                String oem = IOUtils.removeSurroundingSpaces(line.split(OEM_TAG)[1]);
+                currentDevice.setOem(oem);
+            } else if(line.contains(TAG_TAG)) {
+                String tag = IOUtils.removeSurroundingSpaces(line.split(TAG_TAG)[1]);
+                currentDevice.setTag(tag);
+            }
+        }
+    }
+
+    /**
+     * Returns the observer aggregating the program output
+     * @return The observer
+     */
+    public Observer getObserver() {
+        return observer;
+    }
+
+    /**
+     * Returns the parsed list of available devices.
+     * Will be empty if {@link DeviceListParser#parse(Process)} was not called before
+     * @return The available devices list
+     */
+    public List<Device> getDevices() {
+        return devices;
+    }
+}
