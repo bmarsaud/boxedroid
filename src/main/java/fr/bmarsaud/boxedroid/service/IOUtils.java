aca81ad1fe0f2217a8a5dff84b14f72f2fea2cba
Bastien Marsaud <bastien@mouseover.fr>
2022-10-28 18:01:33 +0200

    feat: switch to zip4J to handle zip unix permissions

--- src/main/java/fr/bmarsaud/boxedroid/service/IOUtils.java
+++ src/main/java/fr/bmarsaud/boxedroid/service/IOUtils.java
@@ -1,5 +1,7 @@
 package fr.bmarsaud.boxedroid.service;
 
+import net.lingala.zip4j.ZipFile;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,8 +12,6 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Path;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class IOUtils {
     /**
@@ -29,28 +29,12 @@ public class IOUtils {
 
     /**
      * Unzip all files from a zip file to a destination.
-     * @param zipFile The zip file to unzip.
+     * @param file The zip file to unzip.
      * @param destination The destination where to unzip the archive.
      */
-    public static void unzipFile(File zipFile, Path destination) throws IOException {
-        try(FileInputStream fileInputStream = new FileInputStream(zipFile);
-            ZipInputStream zipInputStream = new ZipInputStream(fileInputStream)) {
-            for(ZipEntry zipEntry = zipInputStream.getNextEntry(); zipEntry != null; zipEntry = zipInputStream.getNextEntry()) {
-                File outputFile = destination.resolve(zipEntry.getName()).toFile();
-                if(zipEntry.isDirectory()) {
-                    if (!outputFile.isDirectory() && !outputFile.mkdirs()) {
-                        throw new IOException("Failed to create directory " + outputFile);
-                    }
-                } else {
-                    File parent = outputFile.getParentFile();
-                    if(!parent.isDirectory() && !parent.mkdirs()) {
-                        throw new IOException("Failed to create directory " + parent);
-                    }
-                    try(FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
-                        fileOutputStream.getChannel().transferFrom(Channels.newChannel(zipInputStream), 0, Long.MAX_VALUE);
-                    }
-                }
-            }
+    public static void unzipFile(File file, Path destination) throws IOException {
+        try(ZipFile zipFile = new ZipFile(file)) {
+            zipFile.extractAll(destination.toString());
         }
     }
 
